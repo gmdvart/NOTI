5a3be320cc180b0b78732c11b01104909e8ed370
extermin4tus <montarstrike777@gmail.com>
2023-05-01 10:59:54 +0300

    initial commit

---
+++ app/src/main/java/com/example/noteapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.noteapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container_view, NoteListFragment.class, null);
+        transaction.commit();
+    }
+}
